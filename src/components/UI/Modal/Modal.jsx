570202de9eb42ed066f0a1446755744c7e8b915c
Nanotorre <fernandodelatorre4@gmail.com>
2019-11-21 15:28:04 +0100

    optimizations

--- src/components/UI/Modal/Modal.jsx
+++ src/components/UI/Modal/Modal.jsx
@@ -1,23 +1,32 @@
-import React from "react";
+import React, { Component } from "react";
 import classes from "./Modal.module.css";
 import Aux from "../../../hoc/Aux";
 import Backdrop from '../Backdrop/Backdrop';
 
-const modal = props => {
-  return (
-    <Aux>
-      <Backdrop show={props.show} click={props.modalClosed}></Backdrop>
-      <div
-        className={classes.Modal}
-        style={{
-          opacity: props.show ? 1 : 0,
-          transform: props.show ? "translateY(0)" : "translateY(-100vh)"
-        }}
-      >
-        {props.children}
-      </div>
-    </Aux>
-  );
+class modal extends Component {
+
+  shouldComponentUpdate (nextProps, nextState) {
+    return nextProps.show !== this.props.show;
+  }
+
+
+  render () {
+    return (
+      <Aux>
+        <Backdrop show={this.props.show} click={this.props.modalClosed}></Backdrop>
+        <div
+          className={classes.Modal}
+          style={{
+            opacity: this.props.show ? 1 : 0,
+            transform: this.props.show ? "translateY(0)" : "translateY(-100vh)"
+          }}
+        >
+          {this.props.children}
+        </div>
+      </Aux>
+    );
+  }
+  
 };
 
 export default modal;
